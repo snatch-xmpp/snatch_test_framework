268ed6adcb2458d5bf8c98d9bbb16284f1b965b7
Manuel Rubio <manuel@altenwald.com>
2017-12-09 16:10:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+
+
+# snatch_test_framework #
+
+Copyright (c) 2017 Manuel Rubio
+
+__Authors:__ "Manuel Rubio" ([`manuel@altenwald.com`](mailto:manuel@altenwald.com)).
+
+[![Build Status](https://img.shields.io/travis/snatch-xmpp/snatch_test_framework/master.svg)](https://travis-ci.org/snatch-xmpp/snatch_test_framework)
+[![Codecov](https://img.shields.io/codecov/c/github/snatch-xmpp/snatch_test_framework.svg)](https://codecov.io/gh/snatch-xmpp/snatch_test_framework)
+[![License: Apache 2.0](https://img.shields.io/github/license/snatch-xmpp/snatch_test_framework.svg)](https://raw.githubusercontent.com/snatch-xmpp/snatch_test_framework/master/LICENSE)
+
+Test Framework for the Lightweight XMPP Client Library for Erlang.
+
+Check [the documentation here](doc/testing.md).
+
+Getting Started
+---------------
+
+If you are using rebar3 it's easy to put in the test profile the following:
+
+```erlang
+{deps, [
+    {snatch_test_framework, "0.1.0"}
+]}
+```
+
+Troubleshooting
+---------------
+
+Feel free to create an issue in github to point a bug, flaw or improvement and even send a pull request with a specific change. Read the [LICENSE](http://github.com/snatch-xmpp/snatch_test_framework/blob/master/doc/LICENSE) if you have doubts about what you can do with the code.
+
+Enjoy!
+
+
+## Modules ##
+
+
+<table width="100%" border="0" summary="list of modules">
+<tr><td><a href="http://github.com/snatch-xmpp/snatch_test_framework/blob/master/doc/snatch_fun_test.md" class="module">snatch_fun_test</a></td></tr></table>
+
---
+++ test/snatch_dummy.erl
@@ -0,0 +1,78 @@
+-module(snatch_dummy).
+
+-export([start_link/0, handle_info/2]).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("fast_xml/include/fxml.hrl").
+-include_lib("snatch/include/snatch.hrl").
+
+-define(JID_USER, <<"bob@localhost/pc">>).
+-define(JID_COMP, <<"alice.localhost">>).
+
+-define(PING_REQUEST,
+        #xmlel{name = <<"iq">>,
+               attrs = [{<<"type">>, <<"get">>},
+                        {<<"from">>, ?JID_USER},
+                        {<<"to">>, ?JID_COMP},
+                        {<<"id">>, <<"test_bot">>}],
+               children = [
+                   #xmlel{name = <<"ping">>,
+                          attrs = [{<<"xmlns">>, <<"urn:xmpp:ping">>}]}
+               ]}).
+-define(PING_RESPONSE,
+        #xmlel{name = <<"iq">>,
+               attrs = [{<<"type">>, <<"result">>},
+                        {<<"to">>, ?JID_USER},
+                        {<<"from">>, ?JID_COMP},
+                        {<<"id">>, <<"test_bot">>}],
+               children = []}).
+
+-define(QUERY_REQUEST,
+        #xmlel{name = <<"iq">>,
+               attrs = [{<<"type">>, <<"get">>},
+                        {<<"from">>, ?JID_USER},
+                        {<<"to">>, ?JID_COMP},
+                        {<<"id">>, <<"test_bot">>}],
+               children = [
+                   #xmlel{name = <<"query">>,
+                          attrs = [{<<"xmlns">>, <<"urn:custom">>}]}
+               ]}).
+-define(QUERY_RESPONSE,
+        #xmlel{name = <<"iq">>,
+               attrs = [{<<"type">>, <<"result">>},
+                        {<<"to">>, ?JID_USER},
+                        {<<"from">>, ?JID_COMP},
+                        {<<"id">>, <<"test_bot">>}],
+               children = [
+                            #xmlel{name = <<"query">>,
+                                   attrs = [{<<"xmlns">>,
+                                             <<"urn:custom">>}],
+                                   children = [
+                                       #xmlel{name = <<"item">>,
+                                              children = [{xmlcdata, <<"abc">>}]}
+                                   ]}
+                          ]}).
+
+%% NOTE: We can use here a gen_server but I think it's overkill for
+%%       the test I want to run.
+start_link() ->
+    PID = spawn_link(fun loop/0),
+    true = register(?MODULE, PID),
+    {ok, PID}.
+
+loop() ->
+    receive
+        stop ->
+            ok;
+        Data ->
+            handle_info(Data, []),
+            loop()
+    end.
+
+handle_info({received, ?QUERY_REQUEST, #via{}}, []) ->
+    snatch:send(fxml:element_to_binary(?QUERY_RESPONSE)),
+    {noreply, []};
+
+handle_info({received, ?PING_REQUEST}, []) ->
+    snatch:send(fxml:element_to_binary(?PING_RESPONSE)),
+    {noreply, []}.
